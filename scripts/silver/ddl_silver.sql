dc3ba0d877f1a5a8bfccf58d63dd56e32e8d3f41
Arif Rumel <75714129+arifrumel400@users.noreply.github.com>
2025-08-17 17:51:57 +0600

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,7 +1,7 @@
 /*
 ===============================================================================
 DDL Script: Create Silver Tables
-===============================================================================
+================================================================================
 Script Purpose:
     This script creates tables in the 'silver' schema, dropping existing tables 
     if they already exist.
